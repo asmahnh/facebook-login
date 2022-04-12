dad8b133fa4b8de71c7ead8bd46113acc211ed86
Asma Hanifah <asmahanifah11@gmail.com>
2022-04-12 15:08:11 +0700

    Demo Facebook

--- facebook/src/facebook/Myfacebook.java
+++ facebook/src/facebook/Myfacebook.java
@@ -1,10 +1,13 @@
 package facebook;
 
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Myfacebook {
 
+@SuppressWarnings("deprecation")
 public static void main(String[] args) {
 		
 	System.setProperty("webdriver.gecko.driver","C:\\selenium-java-4.1.3\\geckodriver-v0.31.0-win64\\geckodriver.exe");
@@ -13,8 +16,7 @@ public static void main(String[] args) {
 
 	driver.get("https://www.facebook.com/");
 	driver.manage().window().maximize();
-	
-	
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	
 	driver.quit();
 
